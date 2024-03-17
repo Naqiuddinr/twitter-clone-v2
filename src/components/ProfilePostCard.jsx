897765e3c0b0fe1cb38fff145c8352da9e9d601b
Naqiuddinr <mnaqiuddin.rsl@gmail.com>
2024-03-18 07:00:44 +0800

    like and unlike function

--- src/components/ProfilePostCard.jsx
+++ src/components/ProfilePostCard.jsx
@@ -1,25 +1,54 @@
 
+import axios from "axios";
+import { jwtDecode } from "jwt-decode";
 import { useEffect, useState } from "react";
 import { Button, Col, Image, Row } from "react-bootstrap";
 
 
 export default function ProfilePostCard({ content, postId }) {
 
-    const [likesCount, setLikesCount] = useState(0);
-    const [like, setLike] = useState(false)
+    const [likes, setLikes] = useState([])
+
+    const token = localStorage.getItem("authToken");
+    const decode = jwtDecode(token);
+    const userId = decode.id;
 
     const pic = 'https://pbs.twimg.com/profile_images/1587405892437221376/h167Jlb2_400x400.jpg';
+    const BASE_URL = "https://85e26881-9d33-47ea-98c8-e946f60ddfca-00-1ciplofky6m83.spock.replit.dev"
 
     useEffect(() => {
-        fetch(`https://85e26881-9d33-47ea-98c8-e946f60ddfca-00-1ciplofky6m83.spock.replit.dev/likes/posts/${postId}`)
+        fetch(`${BASE_URL}/likes/posts/${postId}`)
             .then((response) => response.json())
-            .then((data) => setLikesCount(data.length))
+            .then((data) => setLikes(data))
             .catch((error) => console.error("Error:", error))
     }, [postId])
 
-    async function handleLike() {
-        setLike(!like);
+    const isLiked = likes.some((like) => like.user_id === userId);
 
+    const handleLike = () => (isLiked ? removeFromLikes() : addToLikes());
+
+    const addToLikes = () => {
+        axios.post(`${BASE_URL}/likes`, {
+            user_id: userId,
+            post_id: postId
+        })
+            .then((response) => {
+                setLikes([...likes, { ...response.data, likes_id: response.data.id }])
+            })
+            .catch((error) => console.error("Error: ", error))
+    }
+
+    const removeFromLikes = () => {
+        const like = likes.find((like) => like.user_id === userId);
+
+        if (like) {
+            axios
+                .put(`${BASE_URL}/likes/${userId}/${postId}`)
+                .then(() => {
+                    setLikes(likes.filter((likeItem) => likeItem.user_id !== userId));
+                })
+                .catch((error) => console.error("Error: ", error))
+        }
     }
 
     return (
@@ -46,7 +75,12 @@ export default function ProfilePostCard({ content, postId }) {
                         <i className="bi bi-repeat"></i>
                     </Button>
                     <Button variant="light" onClick={handleLike}>
-                        <i className="bi bi-heart" style={{ color: like ? "red" : "" }}> {likesCount}</i>
+                        {isLiked ? (
+                            <i className="bi bi-heart-fill text-danger"></i>
+                        ) : (
+                            <i className="bi bi-heart"></i>
+                        )}
+                        {likes.length}
                     </Button>
                     <Button variant="light">
                         <i className="bi bi-graph-up"></i>
